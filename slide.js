313a35f1dd50aad22426a5655d71b064277226c5
Bhadrayu09 <127506313+Bhadrayu09@users.noreply.github.com>
2024-08-21 15:30:34 +0530

    baneer changes

--- slide.js
+++ slide.js
@@ -5,44 +5,64 @@ let listHTML = document.querySelector('.carousel .list');
 let seeMoreButtons = document.querySelectorAll('.seeMore');
 let backButton = document.getElementById('back');
 
-nextButton.onclick = function(){
+let autoRotate;
+
+// Function to start auto-rotation
+const startAutoRotate = () => {
+    autoRotate = setInterval(() => {
+        showSlider('nxt');
+    }, 5000); // Adjust the timing as necessary
+};
+
+// Function to stop auto-rotation
+const stopAutoRotate = () => {
+    clearInterval(autoRotate);
+};
+
+// Initial auto-rotation
+startAutoRotate();
+
+nextButton.onclick = function() {
     showSlider('nxt');
-}
-prevButton.onclick = function(){
+};
+
+prevButton.onclick = function() {
     showSlider('prv');
-}
+};
+
 let unAcceppClick;
+
 const showSlider = (type) => {
     nextButton.style.pointerEvents = 'none';
     prevButton.style.pointerEvents = 'none';
 
     carousel.classList.remove('nxt', 'prv');
     let items = document.querySelectorAll('.carousel .list .item');
+    
     if(type === 'nxt'){
         listHTML.appendChild(items[0]);
         carousel.classList.add('nxt');
-    }else{
+    } else {
         listHTML.prepend(items[items.length - 1]);
         carousel.classList.add('prv');
     }
+
     clearTimeout(unAcceppClick);
-    unAcceppClick = setTimeout(()=>{
+    unAcceppClick = setTimeout(() => {
         nextButton.style.pointerEvents = 'auto';
         prevButton.style.pointerEvents = 'auto';
-    }, 2000)
-}
+    }, 2000);
+};
+
 seeMoreButtons.forEach((button) => {
-    button.onclick = function(){
+    button.onclick = function() {
+        stopAutoRotate(); // Stop auto-slide
         carousel.classList.remove('nxt', 'prv');
         carousel.classList.add('showDetail');
-    }
+    };
 });
-backButton.onclick = function(){
+
+backButton.onclick = function() {
     carousel.classList.remove('showDetail');
-}
-
-// auto rotate 
-let autoRotate = setInterval(() => {
-    showSlider('nxt');
-}, 10000);
-
+    startAutoRotate(); // Restart auto-slide
+};
